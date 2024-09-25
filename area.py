182e3730d0929d9fa384fbbe60b551cb9eaa37a1
apurvamagar15 <159618512+apurvamagar15@users.noreply.github.com>
2024-09-25 10:00:27 +0530

    Create area.py

---
+++ area.py
@@ -0,0 +1,9 @@
+areaSquare=lambda length : length * length
+
+areaRect=lambda length,width : length * width
+
+l=int(input("Enter Length Value to calcualte area of Square: "))
+
+print("Area of Square:",areaSquare(l))
+
+l=int(input("\n Enter Length Value to calcualte area of Rectangle:"))
